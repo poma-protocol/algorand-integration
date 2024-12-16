cdd0757d755760fba35bc3fc0b2b49265d1c35ad
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2024-12-16 12:06:54 +0300

    feat: fixing merges

---
+++ site/src/components/footer/index.tsx
@@ -0,0 +1,15 @@
+"use client"
+import Image from "next/image"
+export default function Footer()
+{
+    return (
+        <div>
+            <div>
+
+            </div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    )
+}
