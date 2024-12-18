4f6742fc99bbddf34c3c4215f322fbe7a46d2be5
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2024-12-18 08:39:15 +0300

    fix: Sidebar changes

--- site/src/components/app-sidebar/index.tsx
+++ site/src/components/app-sidebar/index.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { FiSend } from "react-icons/fi";
 import { PiWalletBold } from "react-icons/pi";
 import { MdSavings } from "react-icons/md";
@@ -6,14 +6,15 @@ import SendRewards from "../send-rewards";
 import Treasury from "../treasury";
 import { useState } from "react";
 import FundTreasury from "../fund-treasury";
+
 const Sidebar = () => {
     const [open, setOpen] = useState(true);
-    const [activePage, setActivePage] = useState("Overview");
+    const [activePage, setActivePage] = useState("Treasury Wallet");
 
     const Menus = [
         { title: "Treasury Wallet", Icon: PiWalletBold },
         { title: "Send Rewards", Icon: FiSend },
-        { title: "Fund Treasury", Icon: MdSavings, gap: true }
+        { title: "Fund Treasury", Icon: MdSavings, gap: true },
     ];
 
     const renderContent = () => {
@@ -33,15 +34,17 @@ const Sidebar = () => {
         <div className="flex h-full">
             {/* Sidebar */}
             <div
-                className={`${open ? "w-72" : "w-20"
-                    } bg-white border-r border-black p-5 pt-8 relative duration-300 -z-10`}
+                className={`${
+                    open ? "w-72" : "w-20"
+                } bg-white border-r border-black p-5 pt-8 relative duration-300`}
             >
                 <div className="flex gap-x-4 items-center">
                     <h1
-                        className={`text-black origin-left font-medium text-xl duration-200 ${!open && "scale-0"
-                            }`}
+                        className={`text-black origin-left font-medium text-xl duration-200 ${
+                            !open && "scale-0"
+                        }`}
                     >
-                    
+                        Sidebar
                     </h1>
                 </div>
                 <ul className="pt-6">
@@ -49,13 +52,15 @@ const Sidebar = () => {
                         <li
                             key={index}
                             onClick={() => setActivePage(Menu.title)}
-                            className={`flex rounded-md p-2 cursor-pointer hover:bg-gray-200 text-black text-sm items-center gap-x-4 ${Menu.gap ? "mt-9" : "mt-2"
-                                } ${activePage === Menu.title ? "bg-gray-300" : ""}`}
+                            className={`flex rounded-md p-2 cursor-pointer hover:bg-gray-200 text-black text-sm items-center gap-x-4 ${
+                                Menu.gap ? "mt-9" : "mt-2"
+                            } ${activePage === Menu.title ? "bg-gray-300" : ""}`}
                         >
                             <Menu.Icon size={20} />
                             <span
-                                className={`${!open && "hidden"
-                                    } origin-left duration-200`}
+                                className={`${
+                                    !open && "hidden"
+                                } origin-left duration-200`}
                             >
                                 {Menu.title}
                             </span>
@@ -65,7 +70,7 @@ const Sidebar = () => {
             </div>
 
             {/* Main Content */}
-            <div className="flex-1 p-5 ">
+            <div className="flex-1 p-5">
                 <h2 className="text-2xl font-semibold mb-4">{activePage}</h2>
                 {renderContent()}
             </div>
