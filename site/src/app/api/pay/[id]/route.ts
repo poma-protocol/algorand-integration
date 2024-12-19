e2b087c76442be38a4c69e115324dc8e3d988390
romannjoroge <roman.njoroge@njuguna.com>
2024-12-19 20:40:39 +0300

    feat: Marking prizes as paid complete

--- site/src/app/api/pay/[id]/route.ts
+++ site/src/app/api/pay/[id]/route.ts
@@ -1,3 +1,7 @@
+import { eq } from "drizzle-orm";
+import db from "../../database";
+import { userPrizes } from "../../schema";
+
 export async function POST(
     req: Request,
     { params }: { params: Promise<{ id: string }> }
@@ -5,7 +9,10 @@ export async function POST(
     try {
         const prizeId = Number.parseInt((await params).id);
         
-
+        // Update db
+        await db.update(userPrizes).set({
+            paid: true
+        }).where(eq(userPrizes.id, prizeId));
         return Response.json({message: "Prize Marked As Paid"}, {status: 201});
     } catch(err) {
         console.log("Error Marking Prize As Paid =>", err);
