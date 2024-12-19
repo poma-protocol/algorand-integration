793d325bcae792a52ea8a0495a490a288e4d8ea9
romannjoroge <roman.njoroge@njuguna.com>
2024-12-19 20:13:19 +0300

    feat: v1 of storing complete

--- site/src/api/complete/route.ts
+++ site/src/api/complete/route.ts
@@ -1,4 +1,8 @@
+import { doesAccountExist, getAssetDetails, hasAccountOptedIn } from "@/utils/get-asset-details";
 import {z} from "zod";
+import algodClient from "../algodClient";
+import db from "../database";
+import { userPrizes } from "../schema";
 const completionSchema = z.object({
     userAddress: z.string({message: "User Address Should Be A String"}),
     asset: z.union([z.literal("ALGO"), z.number()], {message: "Invalid Asset"}),
@@ -6,10 +10,38 @@ const completionSchema = z.object({
 })
 export async function POST(req: Request) {
     try {
-        // Store details in db
         const parsed = completionSchema.safeParse(req.json());
         if (parsed.success) {
-            
+            let data = parsed.data
+            // Verify that address exists
+            let accountExist = await doesAccountExist(data.userAddress, algodClient);
+            if (!accountExist) {
+                return Response.json({error: ["User Account Does Not Exist"]}, {status: 400});
+            }
+
+            // Verify that address has opted in
+            if (typeof(data.asset) === "number") {
+                let accountOptIn = await hasAccountOptedIn(algodClient, data.userAddress, data.asset);
+                if (!accountOptIn) {
+                    return Response.json({error: ["User Has Not Opted In To Asset"]}, {status: 400});
+                }
+
+                // Verify that asset exists
+                try {
+                    await getAssetDetails(algodClient, data.asset);
+                } catch(err) {
+                    return Response.json({error: ["Asset Does Not Exist"]}, {status: 400});
+                }
+            }
+
+            // Store details
+            await db.insert(userPrizes)
+                .values({
+                    userAddress: data.userAddress,
+                    assetID: data.asset.toString(),
+                    amount: data.amount,
+                    paid: false
+                });
         } else {
             let errors = parsed.error.issues.map((i) => i.message);
             return Response.json({error: errors}, {status: 400})
