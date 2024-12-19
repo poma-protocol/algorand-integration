cb421739d0f095bf3954ea9c8ff35d5cb46c8eac
romannjoroge <roman.njoroge@njuguna.com>
2024-12-19 20:56:06 +0300

    feat: Adding pagination

--- site/src/app/api/prizes/route.ts
+++ site/src/app/api/prizes/route.ts
@@ -1,11 +1,23 @@
 import { eq } from "drizzle-orm";
 import db from "../database";
 import { userPrizes } from "../schema";
+import { type NextRequest } from "next/server";
 
-export async function GET() {
+export async function GET(req: NextRequest) {
     try {
         // Get prizes where paid is false
-        let prizes = await db.select().from(userPrizes).where(eq(userPrizes.paid, false));
+        const paginationParams = req.nextUrl.searchParams;
+        const page = Number.parseInt(paginationParams.get('page')!);
+        const size = Number.parseInt(paginationParams.get('size')!);
+        let prizes = await db.select({
+            id: userPrizes.id,
+            address: userPrizes.userAddress,
+            amount: userPrizes.amount,
+            assetID: userPrizes.assetID
+        }).from(userPrizes)
+        .where(eq(userPrizes.paid, false))
+        .offset((page - 1) * size)
+        .limit(size)
         return Response.json(prizes, {status: 200});
     } catch(err) {
         console.log("Error Getting Prizes =>", err);
