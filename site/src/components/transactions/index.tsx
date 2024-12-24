59000b56302a1889102583012f5673a750cdcbe0
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2024-12-24 17:12:48 +0300

    fix: pay transaction and mark as paid

--- site/src/components/transactions/index.tsx
+++ site/src/components/transactions/index.tsx
@@ -16,27 +16,31 @@ import {
     TableHeader,
     TableRow,
 } from "@/components/ui/table";
-import Image from "next/image";
+import { contract } from "@/utils/algod-client";
+import { useWallet } from "@txnlab/use-wallet-react";
+import algosdk from "algosdk";
+import axios from "axios";
 interface Transaction {
     id: number;
     address: string;
     amount: number;
-    assetId: number | "ALGO";
-    paid?: boolean; // New field for tracking payment status
+    assetID: number | "ALGO";
 }
 
 export default function Transactions() {
     const [transactions, setTransactions] = useState<Transaction[]>([]);
+    const { activeAddress, algodClient, transactionSigner } = useWallet();
     const [currentPage, setCurrentPage] = useState(1);
     const [pageSize] = useState(5); // Adjust page size as needed
     const [isLoading, setIsLoading] = useState(false);
+
 
     // Fetch transactions with pagination
     const fetchTransactions = async (page: number) => {
         setIsLoading(true);
         try {
-            const response = await fetch(`http://algorand.pomaprotocol.com/api/prizes?page=${page}&size=${pageSize}`);
-            const data = await response.json();
+            const response = await axios.get(`/api/prizes?page=${page}&size=${pageSize}`)
+            const data = await response.data;
             if (response.status === 200 || response.status === 201) {
                 setTransactions(data)
             }
@@ -54,35 +58,117 @@ export default function Transactions() {
     useEffect(() => {
         fetchTransactions(currentPage);
     }, [currentPage]);
-
     // Mark a transaction as paid
     const handleMarkAsPaid = async (id: number) => {
         try {
-            await fetch(`http://algorand.pomaprotocol.com/api/pay`, {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ prizeId: id }),
-            });
+           
+            const response = await axios.post(`/api/pay/${id}`);
+            if (response.status === 200 || response.status === 201) {
+                toast.success("Marked as paid!");
+            }
+            else {
+                toast.error("Failed to mark as paid");
+                return;
+            }
             setTransactions((prev) =>
                 prev.map((tx) =>
                     tx.id === id ? { ...tx, paid: true } : tx
                 )
             );
-            toast.success("Marked as paid!");
         } catch (error) {
             toast.error("Failed to mark as paid");
         }
     };
 
-    // Copy wallet address
+    async function handlePay(values: {
+        tokenType: number | "ALGO", address: string, amount: number, id: number
+    }) {
+        if (!activeAddress) {
+            toast.error("Please connect your wallet");
+            return;
+        }
+        try {
+            if (typeof values.tokenType === "number") {
+                console.log("Sending custom token");
+
+                // Send custom token
+                const suggestedParams = await algodClient.getTransactionParams().do();
+                const atc = new algosdk.AtomicTransactionComposer();
+                atc.addMethodCall({
+                    appID: Number.parseInt(process.env.NEXT_PUBLIC_APP_ID!),
+                    method: contract.getMethodByName("send_reward"),
+                    methodArgs: [
+                        values.amount,
+                        values.address,
+                        values.tokenType
+                    ],
+                    appForeignAssets: [values.tokenType],
+                    sender: activeAddress!,
+                    signer: transactionSigner,
+                    suggestedParams
+                })
+                console.log(atc);
+                const result = await atc.execute(algodClient, 4)
+
+                console.info(`[App] ✅ Successfully sent transaction!`, {
+                    confirmedRound: result.confirmedRound,
+                    txIDs: result.txIDs
+                })
+                toast.success("Rewards sent successfully");
+                handleMarkAsPaid(values.id);
+                console.log("Done");
+                return
+
+
+            }
+            else if (values.tokenType === "ALGO") {
+                console.log("Sending algo");
+                // Send ALGO token
+                const suggestedParams = await algodClient.getTransactionParams().do();
+                const atc = new algosdk.AtomicTransactionComposer();
+                atc.addMethodCall({
+                    appID: Number.parseInt(process.env.NEXT_PUBLIC_APP_ID!),
+                    method: contract.getMethodByName("send_algo_reward"),
+                    methodArgs: [
+                        values.amount * 1000000,
+                        values.address
+                    ],
+                    sender: activeAddress!,
+                    signer: transactionSigner,
+                    suggestedParams
+                })
+
+                const result = await atc.execute(algodClient, 4)
+
+                console.info(`[App] ✅ Successfully sent transaction!`, {
+                    confirmedRound: result.confirmedRound,
+                    txIDs: result.txIDs
+                })
+                console.log("Done");
+                toast.success("Rewards sent successfully");
+                handleMarkAsPaid(values.id);
+                return
+            }
+            else {
+                toast.error("Invalid token type");
+                return;
+            }
+
+        }
+        catch (error) {
+            toast.error("Error sending rewards");
+            console.log("Error", error);
+        }
+    }
+
     const handleCopy = (address: string) => {
         navigator.clipboard.writeText(address);
         toast.success("Copied!");
     };
-
     // Pagination handlers
     const handleNextPage = () => setCurrentPage((prev) => prev + 1);
     const handlePreviousPage = () => setCurrentPage((prev) => Math.max(prev - 1, 1));
+
 
     return (
         <div className="space-y-4">
@@ -96,62 +182,40 @@ export default function Transactions() {
                         <TableHead className="text-left">Asset Type</TableHead>
                         <TableHead className="text-right">Amount</TableHead>
                         <TableHead className="text-center">Mark as Paid</TableHead>
-                        <TableHead className="text-center">Copy Address</TableHead>
+                        <TableHead className="text-center">Copy address</TableHead>
+                        <TableHead className="text-center">Pay</TableHead>
                     </TableRow>
                 </TableHeader>
                 <TableBody>
-                    {isLoading ? (
-                        <TableRow>
-                            <TableCell colSpan={5} className="text-center">
-                                Loading transactions...
+                    {transactions.map((tx) => (
+                        <TableRow key={tx.id} className="hover:bg-gray-50">
+                            <TableCell className="font-medium text-left">
+                                {truncateAddress(tx.address)}
+                            </TableCell>
+                            <TableCell className="text-left">{tx.assetID}</TableCell>
+                            <TableCell className="text-right flex items-center justify-center">
+                                {tx.amount} {tx.assetID === "ALGO" ? <SiAlgorand className="ml-2" /> : tx.assetID}
+                            </TableCell>
+                            <TableCell className="text-center">
+                                <Checkbox id={`checkbox-${tx.id}`} onClick={() => handleMarkAsPaid(tx.id)} />
+                            </TableCell>
+                            <TableCell className="text-center">
+                                <Button variant="outline" size="icon" onClick={() => handleCopy(tx.address)}>
+                                    <GoCopy />
+                                </Button>
+                            </TableCell>
+                            <TableCell className="text-center">
+                                <Button
+                                    variant="secondary"
+                                    onClick={() => handlePay({
+                                        tokenType: tx.assetID, address: tx.address, amount: tx.amount, id: tx.id
+                                    })}
+                                >
+                                    Pay
+                                </Button>
                             </TableCell>
                         </TableRow>
-                    ) : transactions.length === 0 ? (
-                        <TableRow>
-                            <TableCell colSpan={5} className="text-center">
-                                <div className="flex justify-center items-center">
-                                    <Image src="/assets/images/empty-transactions.png"
-                                    alt="Empty transactions image"
-                                    height={200}
-                                    width={200}
-                                    
-                                    />
-                                </div>
-                            </TableCell>
-                        </TableRow>
-                    ) : (
-                        transactions.map((tx) => (
-                            <TableRow
-                                key={tx.id}
-                                className={`hover:bg-gray-50 ${tx.paid ? "bg-green-50" : ""}`}
-                            >
-                                <TableCell className="font-medium text-left">
-                                    {truncateAddress(tx.address)}
-                                </TableCell>
-                                <TableCell className="text-left">{tx.assetId}</TableCell>
-                                <TableCell className="text-right flex items-center justify-end">
-                                    {tx.amount} {tx.assetId === "ALGO" ? <SiAlgorand className="ml-2" /> : tx.assetId}
-                                </TableCell>
-                                <TableCell className="text-center">
-                                    <Checkbox
-                                        id={`checkbox-${tx.id}`}
-                                        checked={tx.paid}
-                                        onChange={() => handleMarkAsPaid(tx.id)}
-                                        disabled={tx.paid}
-                                    />
-                                </TableCell>
-                                <TableCell className="text-center">
-                                    <Button
-                                        variant="outline"
-                                        size="icon"
-                                        onClick={() => handleCopy(tx.address)}
-                                    >
-                                        <GoCopy />
-                                    </Button>
-                                </TableCell>
-                            </TableRow>
-                        ))
-                    )}
+                    ))}
                 </TableBody>
             </Table>
             <div className="flex justify-between items-center">
