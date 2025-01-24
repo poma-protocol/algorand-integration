fa34ce336ee53ec4eadb4de8ea081139df6b3e82
romannjoroge <roman.njoroge@njuguna.com>
2025-01-24 17:33:44 +0300

    feat: History endpoint done

---
+++ site/drizzle/0005_lyrical_mandarin.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "userPrizes" ADD PRIMARY KEY ("id");
--- site/src/app/api/schema.ts
+++ site/src/app/api/schema.ts
@@ -1,7 +1,7 @@
 import { pgTable, real, serial, text, boolean, timestamp } from "drizzle-orm/pg-core";
 
 export const userPrizes = pgTable("userPrizes", {
-    id: serial(),
+    id: serial().primaryKey(),
     userAddress: text().notNull(),
     userid: text().notNull().default(""),
     assetID: text().notNull(),
